0d6207f696e909ed4593b3c1c4e0872f95034867
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-10-03 18:33:52 +0000

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,6 @@
 
 for i in range(122, 96, -2):
     lower = chr(i)
-    print(lower, end='')
-for j in range(89, 64, -2):
-    upper = chr(j)
-    print(upper, end='')
+    for j in range(89, 64, -2):
+        upper = chr(j)
+        print('{}{}'.format(upper, lower), end='')
