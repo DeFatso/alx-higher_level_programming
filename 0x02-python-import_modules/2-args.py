9fa321d664ba26cd1da1e75ba16f097f0dcffc5f
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-10-05 10:21:12 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+argc = len(sys.argv) - 1
+sep = ': '
+
+if argc == 0:
+    print('0 arguments.')
+else:
+    if argc == 1:
+        print('{} argument'.format(argc))
+    else:
+        print('{} arguments'.format(argc))
+
+for i in range(1, len(sys.argv)):
+    argument = sys.argv[i]
+    print(f'{i}{sep}{argument}')
