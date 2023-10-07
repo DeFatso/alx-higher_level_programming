021f63f39c0506e3b80336430ac24089c25b94eb
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-10-07 09:12:28 +0000

    5-no_c.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    _my_list = my_list[::-1]
+    for i in _my_list:
+        print('{:d}'.format(i))
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,7 @@
 #!urs/bin/python3
+def no_c(my_string):
+    new_string = ''
+    for i in my_string:
+        if i.lower() != 'c':
+            new_string += i
+    return new_string
