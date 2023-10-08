3c198b1a3f3235df5d109c4bb3f2c7a61abb4dcb
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-10-08 19:48:16 +0000

    12-switch.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    result = []
+
+    for num in my_list:
+        div_by = my_list[num] % 2 == 0
+        result.append(div_by)
+    return result
