b44eca5f6770efb98e300d40d079a10d80d65a35
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-10-23 21:32:43 +0000

    5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def raise_exception():
+    raise TypeError("This is a custom TypeError")
+
+    try:
+        raise_exception()
+    except TypeError as te:
+        print("Exception raised")
