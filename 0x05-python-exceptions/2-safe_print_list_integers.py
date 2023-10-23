1db5642774fb088fc0e08731762b30236f18e385
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-10-23 15:16:43 +0000

    3-safe_print_division.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for item in my_list:
+            if count >= x:
+                break
+            if isinstance(item, int):
+                print(item, end=" ")
+                count += 1
+    except TypeError:
+        pass
+    print()
+    return count
