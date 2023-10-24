e13c06df9de85cbf35f6056f1ec10eb858373d84
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-10-24 21:36:21 +0000

    3-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Defines a square by size."""
+
+
+class Square:
+
+    def __init__(self, size=0):
+        """Instantiates a new Square object with an optional size."""
+        self.__size = size  # Set the size attribute
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Defines a square by size."""
+
+
+class Square:
+
+    def __init__(self, size=0):
+        """Instantiates a new Square object with an optional size."""
+        self.__size = size  # Set the size attribute
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Calculates and returns the square's area."""
+        return self.__size ** 2
