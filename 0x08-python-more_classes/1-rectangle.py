f676447c98c42990c8772215039d4e3d7e09136d
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-10-30 19:39:51 +0000

    4-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+"""Defines a Rectangle"""
+
+
+class Rectangle:
+    """The Rectangle class"""
+
+    def __init__(self, height=0, width=0):
+        """Initialize a Rectangle instance with optional width and height."""
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle with type and value checks."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle with type and value checks."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
