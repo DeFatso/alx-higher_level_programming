f833477512fcfe1ffabb2864271ac5d3b60436f0
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-11-01 02:58:53 +0000

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-class LockedClass:
-    """ locked class"""
+"""Locked Class module"""
 
+
+class LockedClass:
+    """class with only one attribute"""
     __slots__ = ("first_name",)
-    
-    def __init__(self, first_name):
-        self.first_name = first_name
