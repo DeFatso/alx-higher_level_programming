2486050c69da0dc585d4c82911d77a8371e5f068
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-11-06 14:33:15 +0000

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+"""
+class MyList that inherits from
+"""
+class MyList(list):
+    """ class with method """
 
+    pass
+
+    def print_sorted(self):
+        """ prints sorted list """
+        
+
+        print(sorted(list(self)))
