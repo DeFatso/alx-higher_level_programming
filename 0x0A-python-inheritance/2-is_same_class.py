37a6279a2a15349f4042c741ed3a8ecab9599e69
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-11-06 16:24:17 +0000

    5-base_geometry.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+lfdjmdv; e;fdosilvn efd;ilv
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    returns True if the object is exactly
+    an instance of the specified class
+    """
+
+    return type(obj) is a_class
--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 
+"""
+"""
+class BaseGeometry:
+    """ empty class"""
+
+
+    pass
