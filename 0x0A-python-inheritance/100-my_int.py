3eb97797e95d2306e540b8b5c1fbba63a4bb7564
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-11-06 23:31:19 +0000

    101-add_attribute.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 """
+contains MyInt
 """
-class Square(Rectangle):
 
+
+class MyInt(int):
+    """ inherits from int """
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
