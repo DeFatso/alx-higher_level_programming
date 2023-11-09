ce6f70455ed410e0907165009b3a42d72b5c98ef
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-11-09 02:45:32 +0000

    3-to_json_string.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,11 +3,12 @@
 Write a function that appends a string
 at the end of a text file (UTF8) and
 returns the number of characters added:
-"""
+    """
 
 
 def append_write(filename="", text=""):
-     """Appends a string to the end of a UTF8 text file.
+    """
+    Appends a string to the end of a UTF8 text file.
 
     Args:
         filename (str): The name of the file to append to.
@@ -15,6 +16,6 @@ def append_write(filename="", text=""):
     Returns:
         The number of characters appended.
     """
-     with open(filename, 'a', encoding='utf-8') as file:
+    with open(filename, 'a', encoding='utf-8') as file:
         num_characters_added = file.write(text)
         return num_characters_added
