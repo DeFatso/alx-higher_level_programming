84ebfcc4527222953f26a885e3e8b01333ce9fcf
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-11-09 02:52:52 +0000

    4-from_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,6 +4,7 @@ JSON representation of an object
 """
 import json
 
+
 def to_json_string(my_obj):
     """
     returns the JSON representation of
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+from_ json_ string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """  returns an object """
+
+    return json.loads(my_str)
