99a3e79b802dc028919ba3cb666b56030394cbc6
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-12-13 23:52:32 +0000

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,5 @@
 -- script that lists all shows contained in hbtn_0d_tvshows
 
-USE hbtn_0d_tvshows;
-
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 JOIN tv_show_genres ON tv_shows.show_id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
