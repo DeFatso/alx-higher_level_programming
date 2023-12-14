e460db97519852cf098bf690b198e4656a9d9a2f
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-12-14 02:31:33 +0000

    101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,5 +3,5 @@
 SET @dexter_genre_id := (SELECT GROUP_CONCAT(genre_id) FROM tv_show_genres WHERE show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter'));
 SELECT name
 FROM tv_genres
-WHERE id NOT IN (SELECT DISTINCT genre_id FROM tv_show_genres WHERE genre_id IN (@dexter_genre_ids))
+WHERE id NOT IN (SELECT DISTINCT genre_id FROM tv_show_genres WHERE genre_id IN (@dexter_genre_id))
 ORDER BY name ASC;
