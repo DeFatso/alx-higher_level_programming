30548265aa4bfd58cf3ac4bb8c6a25fcfecdcf4e
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-01-09 18:27:12 +0000

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const Square = require('./5-square.js');
+
+class Square extends Square {
+	constructor(size) {
+		super(size);
+	}
+
+	charPrint(c) {
+		if (c === undefined) {
+			c = 'X';
+		}
+
+		for (let i = 0; i < thi.height; i++) {
+			console.log(c.repeat(this.width));
+		}
+	}
+}
+module.exports = Square;
