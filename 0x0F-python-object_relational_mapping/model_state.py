9e31439436500bd53e651d3a67bcf4fb6234c504
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-01-15 01:19:41 +0000

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-file that contains the class definition of a State
+This module defines the State class for representing states in a database.
 """
+
 import sys
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
@@ -10,12 +11,14 @@ from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-
 class State(Base):
     """
-    inherits from Base
+    Class representing a State in the database.
+
     Attributes:
-        id: Id state
+        id (int): The unique identifier for the state.
+        name (str): The name of the state.
+        cities (relationship): Relationship to the City class.
     """
 
     __tablename__ = 'states'
@@ -24,12 +27,12 @@ class State(Base):
 
     cities = relationship("City", back_populates="state")
 
-"""Get command line arguments"""
+# Get command line arguments
 username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-"""Connect to MySQL server"""
+# Connect to MySQL server
 engine = create_engine('mysql://{}:{}@localhost:3306/{}'.
-                       format(username, password, database),
-                       pool_pre_ping=True)
+        format(username, password, database),
+        pool_pre_ping=True)
 
 Base.metadata.create_all(engine)
