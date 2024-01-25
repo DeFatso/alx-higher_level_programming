ad3f5dad89dbfcf75dc2d4431510a95360c8c93c
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-01-25 12:43:14 +0000

    three lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
-#!/usr/bin/env bash
-#script that takes in a URL, sends a request to that URL, and
-#displays the size of the body of the response
-
-url=$1
-
-response=$(curl -sI $url)
-content_length=$(echo "$response" | grep -i "content-length" | awk '{print $2}' | tr -d '\r\n')
-echo ${content_length}
+#!/bin/bash
+# Script that takes in a URL, sends a request to that URL, and
+url=$1; response=$(curl -sI "$url"); content_length=$(echo "$response" | grep -i "content-length" | awk '{print $2}' | tr -d '\r\n'); echo "${content_length}"
