e2f988123550435e333002886f0c04c1fc5130f0
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-01-25 17:04:52 +0000

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request to that URL
-[ "$#" -eq 0 ] && echo "Usage: $0 <URL>" && exit 1; url=$1; [ $(curl -s -o /dev/null -w "%{http_code}" "$url") -eq 200 ] && curl -s "$url"
+curl -s -w "%{http_code}" $1
