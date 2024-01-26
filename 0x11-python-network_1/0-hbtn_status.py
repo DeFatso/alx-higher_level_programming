1a9b4236c3d51ee8a1ae3c8bd9e3a5650de3877d
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-01-26 13:54:48 +0000

    colon

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,8 @@
 
 from urllib import request
 
-url = "https://alx-intranet.hbtn.io/status"
 if __name__ == "__main__":
-    with urllib.request.urlopen(url) as response
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         content = response.read()
         utf8_content = content.decode('utf-8')
         print("Body response:")
