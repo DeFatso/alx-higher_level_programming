fbca66ab160af0aac4b3ac92b5b0d86a3b9cbd4c
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-01-26 13:50:55 +0000

    indent

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,10 @@ from urllib import request
 
 url = "https://alx-intranet.hbtn.io/status"
 if __name__ == "__main__":
-    with urllib.request.urlopen(url) as resonse
-    content = response.read()
-    utf8_content = content.decode('utf-8')
-    print("Body response:")
-    print(f"\t- type: {type(content)}")
-    print(f"\t- content: {content}")
-    print(f"\t- utf8 content: {utf8_content}")
+    with urllib.request.urlopen(url) as response
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {utf8_content}")
