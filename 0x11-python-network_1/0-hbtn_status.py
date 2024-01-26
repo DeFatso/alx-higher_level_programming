66816c621496a8d94ab1b9882b529965f72b17ba
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-01-26 13:42:04 +0000

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" fetches an url"""
+
+import urllib.requests
+
+url = "https://alx-intranet.hbtn.io/status"
+
+try:
+    with urllib.requests.urlopen(url) as resonse
+    content = response.read()
+    utf8_content = content.decode('utf-8')
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {utf8_content}")
+except urllib.error.URLError as e:
+    print(f"Error during request: {e}")
